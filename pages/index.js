37b191c11a291f4550e288081cb82ac523b80c0b
Fahim-C0D3R <150895968+Fahim-C0D3R@users.noreply.github.com>
2025-04-06 10:19:52 +0600

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,11 @@
 import React from 'react';
+import ChessBoard from '../pages/board.js';  // Adjust path as necessary
 
 const Home = () => {
   return (
     <div>
       <h1>Welcome to the Chess Review App!</h1>
+      <ChessBoard />
       <p>This should be some extra content that appears below the heading.</p>
     </div>
   );
