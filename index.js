c3c104c65363c4684232a99bf4b7b8700ae51e3a
Fahim-C0D3R <150895968+Fahim-C0D3R@users.noreply.github.com>
2025-04-06 08:32:56 +0600

    Add files via upload

---
+++ index.js
@@ -0,0 +1,7 @@
+export default function Home() {
+  return (
+    <div className="h-screen flex items-center justify-center bg-black text-white text-4xl">
+      <a href="/board" className="bg-white text-black px-8 py-4 rounded hover:bg-gray-200 transition">Start</a>
+    </div>
+  )
+}
