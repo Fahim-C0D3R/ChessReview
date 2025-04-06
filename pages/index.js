82b06b8f4323d032b4e7d94fb276fb9f096a8048
Fahim-C0D3R <150895968+Fahim-C0D3R@users.noreply.github.com>
2025-04-06 10:16:05 +0600

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,12 @@
 import React from 'react';
 
 const Home = () => {
-  return <div>Welcome to the Chess Review App!</div>;
+  return (
+    <div>
+      <h1>Welcome to the Chess Review App!</h1>
+      <p>This should be some extra content that appears below the heading.</p>
+    </div>
+  );
 };
 
 export default Home;
